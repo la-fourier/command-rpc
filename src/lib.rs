70109dbedd80eec922733fa5e50523231d6f2ba0
Johannes Loos <loos-johannes@gmx.de>
2023-05-04 06:45:28 +0000

    forward all in one macro

--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,66 @@
 // extern crate quote;
 // extern crate syn;
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 //use syn::Item;
-use syn::parse_macro_input;
+use syn::{self, Item, Item::*, spanned::Spanned};
 use std::fmt::Display;
 use std::fs::File;
 use std::io::Write;
+
+
+#[proc_macro_attribute]
+pub fn crpc(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the input tokens into a Rust syntax tree
+    let item = syn::parse_macro_input!(item as syn::Item);
+    match item {
+        Fn(item) => { // For fn
+            todo!()
+        },
+        Mod(item) => { // For mod
+            todo!()
+        },
+        Struct(item) => { // For param test
+            todo!()
+        },
+        _ => {
+            eprint!("Error in {}: crpc can only be used on fn, mod and struct", item.to_token_stream().to_string().lines().next());
+        },
+    }
+
+    // Modify the syntax tree as needed
+    // For example, you can add additional code or metadata to the item
+    // In this simple example, we are just generating a debug print statement
+
+    eprintln!("Expansion sucessfull!");
+    eprintln!("Expansion sucessfull!");
+    eprintln!("Expansion sucessfull!");
+    eprintln!("Expansion sucessfull!");
+    eprintln!("Expansion sucessfull!");
+    eprintln!("Expansion sucessfull!");
+
+    if let Ok(mut file) = File::create("foo.txt") {
+        let res = file.write_all(b"Hello, world!");
+        if let Ok(re) = res {
+            println!("Ok");
+            println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+        } else {
+            println!("Not ok");
+        }
+    }
+
+    // Generate the output tokens
+    let output = quote! {
+        // Add a debug print statement
+        fn pre() {
+            let x = 10;
+        }
+        #item
+    };
+
+    // Return the output tokens as a TokenStream
+    output.into()
+}
 
 
 /// This is where the magic happens.
